959b4f680845e1886ed249a5c45aad6f48ba9c91
Joan Vilarrasa <joan.vilarrasa96@gmail.com>
2025-11-16 18:39:30 +0100

    modified menu

--- install/all.sh
+++ install/all.sh
@@ -3,5 +3,6 @@ JVVESPER_THEME_NAME="jvvesper"
 JVVESPER_THEME_PATH="$THEMES_DIR$JVVESPER_THEME_NAME"
 export JVVESPER_THEME_PATH
 
-$JVVESPER_THEME_PATH/install/packages/install.sh
+# $JVVESPER_THEME_PATH/install/packages/install.sh
+$JVVESPER_THEME_PATH/install/omarchy/install.sh
 $JVVESPER_THEME_PATH/install/waybar/install.sh
---
+++ install/omarchy/install.sh
@@ -0,0 +1,6 @@
+cp $JVVESPER_THEME_PATH/install/omarchy/omarchy-menu ~/.local/share/omarchy/bin/omarchy-menu
+
+cp $JVVESPER_THEME_PATH/install/omarchy/about.txt ~/.local/share/omarchy/icon.txt
+cp $JVVESPER_THEME_PATH/install/omarchy/about.txt ~/.config/omarchy/branding/about.txt
+cp $JVVESPER_THEME_PATH/install/omarchy/screensaver.txt ~/.local/share/omarchy/logo.txt
+cp $JVVESPER_THEME_PATH/install/omarchy/screensaver.txt ~/.config/omarchy/branding/screensaver.txt

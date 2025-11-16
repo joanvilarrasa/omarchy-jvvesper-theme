a8203fa934d30d3ead6e9cf9651ce110c302edbc
Joan Vilarrasa <joan.vilarrasa96@gmail.com>
2025-11-16 16:48:26 +0100

    waybar configuration complete

---
+++ install/all.sh
@@ -0,0 +1,6 @@
+THEMES_DIR="$HOME/.config/omarchy/themes/"
+JVVESPER_THEME_NAME="jvvesper"
+JVVESPER_THEME_PATH="$THEMES_DIR$JVVESPER_THEME_NAME"
+export JVVESPER_THEME_PATH
+
+$JVVESPER_THEME_PATH/install/waybar/install.sh
---
+++ install/waybar/install.sh
@@ -0,0 +1,4 @@
+cp -f $JVVESPER_THEME_PATH/install/waybar/config.jsonc ~/.config/waybar/config.jsonc
+cp -f $JVVESPER_THEME_PATH/install/waybar/style.css ~/.config/waybar/style.css
+
+omarchy-restart-waybar

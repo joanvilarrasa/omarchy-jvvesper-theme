76549a2fd7d55bfe35888f9db51bcf49895153d1
Joan Vilarrasa <joan.vilarrasa96@gmail.com>
2025-11-17 20:48:04 +0100

    fixed kb layout and a bunch ofthings about the theme

--- install/omarchy/install.sh
+++ install/omarchy/install.sh
@@ -1,3 +1,10 @@
 cp $JVVESPER_THEME_PATH/install/omarchy/omarchy-menu ~/.local/share/omarchy/bin/omarchy-menu
+cp $JVVESPER_THEME_PATH/install/omarchy/omarchy-hyprland-workspace-toggle-gaps ~/.local/share/omarchy/bin/omarchy-hyprland-workspace-toggle-gaps
+cp $JVVESPER_THEME_PATH/install/omarchy/omarchy-theme-set-vscode ~/.local/share/omarchy/bin/omarchy-theme-set-vscode
+
+cp $JVVESPER_THEME_PATH/install/omarchy/bindings.conf ~/.config/hypr/bindings.conf
+cp $JVVESPER_THEME_PATH/install/omarchy/input.conf ~/.config/hypr/input.conf
+cp $JVVESPER_THEME_PATH/install/omarchy/looknfeel.conf ~/.config/hypr/looknfeel.conf
+
 rm -f ~/.local/share/applications/Alacritty.desktop
 rm -f ~/.local/share/applications/typora.desktop

5dfcc6f7e953ccb63f60445d3dbc1ec942a6c4ab
Joan Vilarrasa <joan.vilarrasa96@gmail.com>
2026-05-27 20:28:07 +0200

    done some things

--- install/omarchy/install.sh
+++ install/omarchy/install.sh
@@ -1,4 +1,3 @@
-cp $JVVESPER_THEME_PATH/install/omarchy/omarchy-menu ~/.local/share/omarchy/bin/omarchy-menu
 cp $JVVESPER_THEME_PATH/install/omarchy/omarchy-hyprland-workspace-toggle-gaps ~/.local/share/omarchy/bin/omarchy-hyprland-workspace-toggle-gaps
 
 cp $JVVESPER_THEME_PATH/install/omarchy/bindings.conf ~/.config/hypr/bindings.conf
